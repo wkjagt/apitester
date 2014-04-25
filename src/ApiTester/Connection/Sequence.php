f217e78dc81263d1cb248037305ebd4e052681ff
wkjagt <wkjagt@gmail.com>
2014-04-24 21:20:32 -0400

    make previous request variables available in next

--- src/ApiTester/Connection/Sequence.php
+++ src/ApiTester/Connection/Sequence.php
@@ -45,9 +45,13 @@ class Sequence
     public function run()
     {
         $errors = [];
+        $response = null;
 
         foreach($this->spec->get('requests') as $requestDetails) {
 
+            if($response) {
+                $requestDetails = $this->populateFromPreviousResponse($requestDetails, $response);
+            }
             $details = new ArrayAccess($requestDetails);
             $request = new Request($this->client, $details, $this->variables);
 
@@ -58,4 +62,26 @@ class Sequence
         }
         return $errors;
     }
+
+    // todo: this doesn't belong here
+    protected function populateFromPreviousResponse($requestDetails, $response)
+    {
+        array_walk_recursive($requestDetails, function(&$item, $key, $response)
+        {
+            $pattern = '/%previous_request\.(?P<format>\w+)_data\.(?P<key>[a-zA-Z0-9-_\.]+)%/';
+            $item = preg_replace_callback($pattern, function($matches) use ($response)
+            {
+                $body = (string) $response->getBody();
+                // todo make this more flexible
+                switch($matches['format']) {
+                    case 'json':
+                        $data = new ArrayAccess(json_decode($body, true));
+                        return $data->get($matches['key']);
+                }
+            }, $item);
+        }, $response);
+
+        return $requestDetails;
+    }
+
 }
